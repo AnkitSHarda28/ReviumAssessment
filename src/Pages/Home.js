aad50d461c6208e5f65cd41af18d8c6553b716af
AnkitSHarda28 <Ankitsharda68@gmail.com>
2022-07-25 00:43:41 +1000

    added final content to home + package fixing

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,74 @@
 import React from "react";
 import Carousel from "../Components/Carousel";
 import { Images } from "../Components/Images";
+import "./Home.css";
 
 //Component for rendering Home page
 function Home() {
   return (
     <div>
       <Carousel slides={Images} />
+      <div className="homeContainer">
+        <h1>Welcome</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid fugit
+          laborum odio fuga eius. Libero dolorem, placeat nihil ab incidunt
+          magnam iste numquam dignissimos labore. Nesciunt unde, ipsa porro
+          veritatis officiis quisquam. Magnam ipsam fuga, quis consectetur iste
+          autem sint tempore repudiandae velit nulla minima unde exercitationem,
+          atque voluptatibus suscipit voluptatum sed? Eum commodi accusantium at
+          fuga, placeat obcaecati sunt odit reiciendis dicta reprehenderit nihil
+          debitis id. Qui obcaecati accusantium nesciunt voluptatibus! Error
+          eius reiciendis, rem a hic velit fuga dolores blanditiis nihil
+          dignissimos ducimus inventore quibusdam! Non facere, ducimus,
+          accusamus commodi eius sit consectetur atque quae impedit nesciunt
+          minus porro quibusdam ut consequuntur repellat nam animi facilis
+          consequatur saepe libero enim sequi! Quisquam est perferendis soluta
+          ipsum rem, placeat nam, sequi rerum minus libero laborum architecto
+          magnam exercitationem, ad nobis ducimus repudiandae laudantium
+          repellendus modi delectus! Tempore esse aspernatur molestiae, mollitia
+          pariatur amet repellat quod explicabo quos vero asperiores corrupti
+          minus error, doloremque, voluptatem cum fugiat sed quae ab laudantium
+          aliquam nulla molestias nemo. Voluptatum, a. Voluptatem nostrum
+          assumenda, in vel id provident laborum? Sunt numquam eius provident,
+          expedita impedit neque quisquam quo natus consequatur alias animi est
+          nisi beatae quia tempora hic distinctio similique nulla voluptate
+          quibusdam? Aliquam sequi fuga quae sapiente nam dolore molestiae at
+          assumenda possimus, numquam asperiores quia reprehenderit deserunt
+          voluptatem illo cum, alias accusamus id distinctio, vel laboriosam!
+          Architecto mollitia, quaerat earum eligendi nostrum non facilis
+          corrupti deserunt inventore numquam laborum, possimus eos illum vero
+          voluptates beatae nulla. Laudantium qui temporibus facere, asperiores
+          molestiae voluptatum! Iusto veniam corporis repudiandae incidunt
+          quaerat ut itaque laborum esse, porro at dolore doloremque inventore
+          praesentium numquam commodi. Exercitationem consectetur ducimus,
+          soluta quam doloribus vero error veniam voluptatibus non cum hic
+          tempore eligendi rerum animi? Sequi, sed vitae. Repellat autem
+          similique repellendus ipsam illum minus odio laboriosam! Impedit
+          eligendi deserunt perspiciatis ipsa ipsum temporibus quis eaque quidem
+          et consequatur. Minus, incidunt! Laboriosam accusamus at nobis nam
+          perspiciatis ullam animi, rem odio reiciendis harum incidunt libero,
+          perferendis ea quae nemo voluptate, ducimus quis ad facere eos qui.
+          Earum labore soluta, voluptas placeat dolore rerum, esse eligendi
+          animi delectus, dolorum assumenda? Velit recusandae quasi eum quia
+          incidunt culpa expedita aspernatur possimus omnis, natus, est minima
+          veniam dolor tenetur. Asperiores quaerat voluptatem perspiciatis atque
+          aperiam cupiditate laborum iure tempore, repellendus odio rem possimus
+          veniam sed, consequatur saepe voluptatum dolorem assumenda eos ullam
+          debitis? Quis debitis, odit consequuntur, dolores quisquam deleniti
+          maiores quaerat adipisci tempore ullam eaque. Rerum laboriosam maxime
+          error quos modi nisi obcaecati quo iste libero, veniam autem aliquid
+          possimus distinctio? Sapiente nemo minus adipisci illum ipsa ducimus
+          aperiam, cumque accusantium veritatis magni unde inventore in. Facilis
+          consectetur vitae ea repellat in ab corporis deserunt velit. Nostrum
+          nemo libero iusto porro et aperiam quam eius omnis! Quam explicabo,
+          repellendus id doloribus minus quibusdam iusto, porro ab dignissimos
+          est architecto eius animi dolores perspiciatis velit aspernatur fugit
+          nobis. Mollitia, ad et? Amet recusandae deserunt praesentium ut soluta
+          hic explicabo ipsam quia, ducimus totam voluptatem a fuga, aliquam
+          vero corporis. Distinctio, alias numquam?
+        </p>
+      </div>
     </div>
   );
 }
